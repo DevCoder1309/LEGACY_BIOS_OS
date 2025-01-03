0e255d4b06c2e1b419b1a11cb5f22ade1324f37b
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2025-01-03 12:56:36 -0500

    Hello world message added on boot from floppy

--- src/main.asm
+++ src/main.asm
@@ -1,12 +1,66 @@
-org 0x7C00            ; Set origin to 0x7C00
-bits 16               ; 16-bit real mode
+org 0x7C00
+bits 16
+
+
+%define ENDL 0x0D, 0x0A
+
+
+start:
+    jmp main
+
+
+;
+; Prints a string to the screen
+; Params:
+;   - ds:si points to string
+;
+puts:
+    ; save registers we will modify
+    push si
+    push ax
+    push bx
+
+.loop:
+    lodsb               ; loads next character in al
+    or al, al           ; verify if next character is null?
+    jz .done
+
+    mov ah, 0x0E        ; call bios interrupt
+    mov bh, 0           ; set page number to 0
+    int 0x10
+
+    jmp .loop
+
+.done:
+    pop bx
+    pop ax
+    pop si    
+    ret
+    
 
 main:
-    hlt               ; Halt the CPU (simple functionality for testing)
+    ; setup data segments
+    mov ax, 0           ; can't set ds/es directly
+    mov ds, ax
+    mov es, ax
+    
+    ; setup stack
+    mov ss, ax
+    mov sp, 0x7C00      ; stack grows downwards from where we are loaded in memory
+
+    ; print hello world message
+    mov si, msg_hello
+    call puts
+
+    hlt
 
 .halt:
-    jmp .halt         ; Infinite loop to keep the CPU busy
+    jmp .halt
 
-times 510-($-$$) db 0 ; Pad the rest of the boot sector to 510 bytes
-dw 0xAA55             ; Boot signature (must be at offset 0x1FE)
 
+
+msg_hello: db 'Hello world!', ENDL, 0
+
+
+times 510-($-$$) db 0
+dw 0AA55h
